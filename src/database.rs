79db2e98cbea9cdd6c41c3145534149f9b655831
antonovmike <antonovma@outlook.com>
2023-05-12 10:53:49 +0500

    calamine spreadsheet reader

--- src/database.rs
+++ src/database.rs
@@ -1,4 +1,4 @@
-use sqlite::State;
+use calamine::{open_workbook_auto, Reader};
 
 #[derive(Debug, Clone)]
 pub struct Base {
@@ -11,22 +11,21 @@ pub struct Base {
 }
 
 pub async fn base_data(typ: &str) -> Vec<Base> {
-    let connection = sqlite::open("db.sqlite3").unwrap();
-    // The variable typ should contain the name of the catalog from the database:
-    // "catalog_museum" or "catalog_cafe"
-    let query = &format!("SELECT * FROM {}", typ)[..];
-    let mut statement = connection.prepare(query).unwrap();
+    let path = "./data.ods";
+    let mut workbook = open_workbook_auto(path).unwrap();
+    let sheet_name = workbook.sheet_names()[0].to_owned();
+    let range = workbook.worksheet_range(&sheet_name).unwrap();
 
     let mut base_filds: Vec<Base> = vec![];
 
-    while let Ok(State::Row) = statement.next() {
+    for row in range.expect("REASON").rows() {
         let temp_sctruct = Base {
-            name: statement.read::<String, _>("name").unwrap(),
-            summ: statement.read::<String, _>("summary").unwrap(),
-            sche: statement.read::<String, _>("schedule").unwrap(),
-            ggle: statement.read::<String, _>("map_google").unwrap(),
-            lttd: statement.read::<f64, _>("latitude").unwrap(),
-            lngt: statement.read::<f64, _>("longitude").unwrap(),
+            name: row[0].to_string(),
+            summ: row[1].to_string(),
+            sche: row[2].to_string(),
+            ggle: row[3].to_string(),
+            lttd: row[4].to_string().parse::<f64>().unwrap(),
+            lngt: row[5].to_string().parse::<f64>().unwrap(),
         };
         base_filds.push(temp_sctruct)
     }
